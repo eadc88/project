066fe422ada37841cbfbea42a507e13803e702bd
eadc88 <eadc1988@gmail.com>
2022-02-16 22:34:43 -0500

    Gitclass003

---
+++ src/Git/Testing.java
@@ -0,0 +1,14 @@
+package Git;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("we we are were");
+
+	}
+
+}
